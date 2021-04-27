5c82fef7b322a23985ab99775abd8730e9b6fcd6
Yael Anaya <yaelanaya@yaels-mbp.lan>
2021-04-27 14:28:12 -0700

    Initial commit

---
+++ src/main/java/Mastermind/Feedback.java
@@ -0,0 +1,5 @@
+package Mastermind;
+
+//En esta clase se lleva la retroalimentción del juego
+public class Feedback {
+}
---
+++ src/main/java/Mastermind/MasterCode.java
@@ -0,0 +1,5 @@
+package Mastermind;
+
+//Aquí escribiremos la lógica del juego
+public class MasterCode {
+}
